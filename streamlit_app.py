f27ae2b1a3d98b11bf6d79d2a6d3502133f3ba92
TomvanBreugel <160640144+TomvanBreugel@users.noreply.github.com>
2024-02-26 19:47:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,30 +15,27 @@ def main():
             df = pd.read_csv(uploaded_file)
             
             # Display the raw data if needed
-            if st.checkbox("Show Raw Data"):
+            if st.checkbox("Ruwe data"):
                 st.write(df)
             
             # Display basic statistics
-            st.subheader("Basic Statistics")
+            st.subheader("Algemene getallen")
             st.write(df.describe())
             
             # Display column selection
-            selected_columns = st.multiselect("Select columns to display", df.columns)
+            selected_columns = st.multiselect("Selecteer een colom", df.columns)
             if selected_columns:
                 st.write(df[selected_columns])
                 
             # Display summary statistics for selected column
-            selected_column = st.selectbox("Select a column for summary statistics", df.columns)
+            selected_column = st.selectbox("Samenvatting", df.columns)
             if selected_column:
                 st.write(df[selected_column].describe())
                 
             # Display a bar chart for selected column
-            bar_chart_column = st.selectbox("Select a column for bar chart", df.columns)
+            bar_chart_column = st.selectbox("Bar chart", df.columns)
             if bar_chart_column:
                 st.bar_chart(df[CO2 Emissions(g/km)].value_counts())
-                
-        except Exception as e:
-            st.error(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     main()
