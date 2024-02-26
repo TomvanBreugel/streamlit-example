f13ea43fd09c39527dc0f855f31478d548cbe8b9
TomvanBreugel <160640144+TomvanBreugel@users.noreply.github.com>
2024-02-26 16:52:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,4 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-st.set_page_config(page_title="Dashboard",
-                  page_icon)
+st.set_page_config(page_title="Dashboard")
